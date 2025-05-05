2d258ecec3a0091f8df9e3429d82a2da499e1031
FrjlGAM <frijmagpulong@gmail.com>
2025-05-06 00:35:43 +0800

    gmeet ey

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/LoginPage"; 
 import RegistrationPage from "./components/RegistrationPage";
 import Terms from "./components/Terms"; 
-import UnverifiedHomepage from "./components/unverified/UnverifiedHomepage";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/terms" element={<Terms />} />
-        <Route path="/unverifiedhomepage" element={<UnverifiedHomepage />} /> 
         
       </Routes>
     </Router>
